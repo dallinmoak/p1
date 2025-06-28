b2b3f25c0f8e60a49d6f489210d85cdcbea777e1
Dallin Moak (MSI ubuntu) <dallinmoak@gmail.com>
2025-06-28 15:54:48 -0600

    mostly finish streach challenge and formatted graph x axis to not have commas

--- p1_source.py
+++ p1_source.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from lets_plot import *
+from lets_plot import ggsize
 
 LetsPlot.setup_html(isolated_frame=True)
 
@@ -13,34 +14,46 @@ target_name = "Dallin"
 my_query = f'name == "{target_name}"'
 my_name_plot_data = df.query(my_query)[cols_to_print]
 
-my_name_plot = ggplot(
-    data=my_name_plot_data,
-    mapping=aes(
-        x="year",
-        y="Total",
-    ),
-) + geom_bar(stat="identity")
+my_name_plot = (
+    ggplot(
+        data=my_name_plot_data,
+        mapping=aes(
+            x="year",
+            y="Total",
+        ),
+    )
+    + geom_bar(stat="identity")
+    + scale_x_continuous(format="d")
+)
 
 brittany_plot_data = df.query('name == "Brittany"')[cols_to_print]
 
 brittany_plot_data["age"] = 2025 - brittany_plot_data["year"]
 
-brittany_plot = ggplot(
-    data=brittany_plot_data,
-    mapping=aes(
-        x="age",
-        y="Total",
-    ),
-) + geom_bar(stat="identity")
+brittany_plot = (
+    ggplot(
+        data=brittany_plot_data,
+        mapping=aes(
+            x="age",
+            y="Total",
+        ),
+    )
+    + geom_bar(stat="identity")
+    + scale_x_continuous(format="d")
+)
 
 q3_data = df.query(
     'name in ["Mary", "Martha", "Peter", "Paul"] and 1920 <= year <= 2000'
 )[["year", "name", "Total"]].rename(columns={"name": "Name"})
 
-q3_data_plot = ggplot(
-    data=q3_data,
-    mapping=aes(x="year", y="Total", color="Name"),
-) + geom_line(stat="identity")
+q3_data_plot = (
+    ggplot(
+        data=q3_data,
+        mapping=aes(x="year", y="Total", color="Name"),
+    )
+    + geom_line(stat="identity")
+    + scale_x_continuous(format="d")
+)
 
 q3_data_alt = q3_data.copy()
 
@@ -54,19 +67,46 @@ def compute_relative(row):
 
 q3_data_alt["Relative"] = q3_data_alt.apply(compute_relative, axis=1)
 
-q3_data_plot_alt = ggplot(
-    data=q3_data_alt,
-    mapping=aes(x="year", y="Relative", color="Name"),
-) + geom_line(stat="identity")
+q3_data_plot_alt = (
+    ggplot(
+        data=q3_data_alt,
+        mapping=aes(x="year", y="Relative", color="Name"),
+    )
+    + geom_line(stat="identity")
+    + scale_x_continuous(format="d")
+)
 
 movie_name = r"(^|.+[\s-])Neo($|[\s-].+)"
 
 movie_name_data = df[df["name"].str.contains(movie_name, case=False)][cols_to_print]
 
-movie_name_plot = ggplot(
-    data=movie_name_data,
-    mapping=aes(
-        x="year",
-        y="Total",
-    ),
-) + geom_bar(stat="identity")
+movie_name_plot = (
+    ggplot(
+        data=movie_name_data,
+        mapping=aes(
+            x="year",
+            y="Total",
+        ),
+    )
+    + geom_bar(stat="identity")
+    + scale_x_continuous(format="d")
+)
+
+elliot_data = df.query('name == "Elliot"')[cols_to_print]
+elliot_data["year"] = elliot_data["year"].astype(float)
+
+elliot_plot = (
+    ggplot(
+        data=elliot_data,
+        mapping=aes(x="year", y="Total", color="name"),
+    )
+    + geom_line(stat="identity")
+    + labs(color="name")
+    + ggtitle("Elliot... What?")
+    + geom_vline(xintercept=1982, color="blue", linetype="dashed")
+    + geom_vline(xintercept=1985, color="blue", linetype="dashed")
+    + geom_vline(xintercept=2002, color="blue", linetype="dashed")
+    + scale_x_continuous(limits=[1950, 2025])
+    + ggsize(width=700, height=400)
+    + scale_x_continuous(format="d")
+)
